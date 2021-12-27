efa9c5b25442370b39609efd089d352e86ac73ed
zsoltjb <zjb@digitalrifle.com>
2021-12-27 21:16:45 +0000

    Task completed
    All files and libraries are done

---
+++ task/libs/js/script.js
@@ -0,0 +1,128 @@
+	$(window).on('load', function () {
+		if ($('#preloader').length) {
+				$('#preloader').delay(1000).fadeOut('slow', function () {
+				$(this).remove();
+			});
+		}
+	});
+
+	
+	$('#submit1').click(function() {
+		
+		let query = $('#wikiSearch').val();
+		
+		$.ajax({
+			url: "libs/php/getWikipediaSearch.php",
+			type: 'POST',
+			dataType: 'json',
+			data: {
+				query: query
+			},
+			success: function(result) {
+
+				console.log(JSON.stringify(result));
+
+				if (result.status.name == "ok") {
+					$('#results').html("");
+					for (i = 0; i <= result['data'].length-1; i++){
+						$('#results').append(`
+						<h2>About ${result['data'][i]['title']}</h2>
+						<button onclick="copyFunc()" class="copy" data-lat="${result['data'][i]['lat']}" data-lng="${result['data'][i]['lng']}">Copy coordinates</button>
+						<br><br>
+						${result['data'][i]['summary']} <br><br>
+						<a href="https://${result['data'][i]['wikipediaUrl']}" target="_blank" >Click here to read more</a><hr>
+						`)
+					};
+				}
+			
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				console.log("no success\n" + errorThrown + "\n" + textStatus + "\n" + jqXHR);
+			}
+		}); 
+	
+	});
+	
+	$('#submit2').click(function() {
+
+		$.ajax({
+			url: "libs/php/getWeatherObservation.php",
+			type: 'POST',
+			dataType: 'json',
+			data: {
+				lat: $('#lat').val(),
+				lng: $('#lng').val()
+			},
+			success: function(result) {
+
+				console.log(JSON.stringify(result));
+
+				if (result.status.name == "ok") {
+
+					$('#results').html(`
+					<h2>Most recent weather observation</h2> <br><br>
+					Observation station's name: ${result['data']['weatherObservation']['stationName']} <br>
+					Temperature: ${result['data']['weatherObservation']['temperature']}\u2103 <br>
+					Clouds: ${result['data']['weatherObservation']['clouds']} <br>
+					Humidity: ${result['data']['weatherObservation']['humidity']}% <br>
+					Wind speed: ${result['data']['weatherObservation']['windSpeed']} mph <br>
+					Time of observation: ${result['data']['weatherObservation']['datetime']}
+					`);
+				}
+			
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				console.log("no success\n" + errorThrown + "\n" + textStatus + "\n" + jqXHR);
+			}
+		}); 
+	
+	});
+	
+	$('#submit3').click(function() {
+			
+		let lat = $('#lat-2').val();
+		let lng = $('#lng-2').val();
+		let radius = $('#radius').val();
+
+		$.ajax({
+			url: "libs/php/getOceanName.php",
+			type: 'POST',
+			dataType: 'json',
+			data: {
+				lat: lat,
+				lng: lng,
+				radius: radius
+			},
+			success: function(result) {
+
+				console.log(JSON.stringify(result));
+
+				if (result.status.name == "ok") {
+
+					$('#results').html(`
+					<h2>Sea or Ocean near within given radius</h2> <br><br>
+					The ${result['data']['ocean']['name']} is<br>
+					${result['data']['ocean']['distance']} miles away from the above location.
+					`);
+				}
+			
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				console.log("no success\n" + errorThrown + "\n" + textStatus + "\n" + jqXHR);
+			}
+		}); 
+	
+	});
+	
+	copyFunc = () => {
+		$('#lat').val($(event.target).data('lat'));
+		$('#lng').val($(event.target).data('lng'));
+		$('#lat-2').val($(event.target).data('lat'));
+		$('#lng-2').val($(event.target).data('lng'));
+	};
+	
+	
+	
+	$('#radius').change(function() {
+		$('#radiusLabel').html($('#radius').val());
+	});
---
+++ task/libs/php/getOceanName.php
@@ -0,0 +1,38 @@
+<?php
+
+	// remove for production
+
+	ini_set('display_errors', 'On');
+	error_reporting(E_ALL);
+
+	$executionStartTime = microtime(true);
+	
+	$lat = $_REQUEST['lat'];
+	$lng = $_REQUEST['lng'];
+	$radius = $_REQUEST['radius'];
+
+	$url='http://api.geonames.org/oceanJSON?lat=' . $lat . '&lng=' . $lng . '&radius=' . $radius . '&username=zsoltjb';
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL,$url);
+
+	$result=curl_exec($ch);
+
+	curl_close($ch);
+
+	$decode = json_decode($result,true);	
+
+	$output['status']['code'] = "200";
+	$output['status']['name'] = "ok";
+	$output['status']['description'] = "success";
+	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+	$output['data'] = $decode;
+	
+	/*header('Access-Control-Allow-Origin: http://zsoltjbea.co.uk/');*/
+	header('Content-Type: application/json; charset=UTF-8');
+
+	echo json_encode($output);
+
+?>
